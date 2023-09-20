fc8597b7883d9d03f31cb49a1f5808c3cc22d533
Henryikenna <ikennaunegbu10@gmail.com>
2023-09-20 19:44:41 +0100

    One-Marvellous-#17

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hng_events_app/constants/colors.dart';
-import 'package:hng_events_app/screens/dash_board_screen.dart';
-import 'package:hng_events_app/navigation/navigation_manager.dart';
-
+import 'package:hng_events_app/screens/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,26 +12,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    
-    return ScreenUtilInit(
-        designSize: const Size(370, 810),
-        minTextAdapt: false,
-        splitScreenMode: true,
-        builder: (_, child) {
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            title: 'Flutter Demo',
-            theme: ThemeData(
-              colorScheme: ColorScheme.fromSeed(
-                seedColor: const Color(0xffE78DFB),
-                primary: ProjectColors.purple,
-                // secondary: const Color(0xFFF97316),
-              ),
-              useMaterial3: true,
-            ),
-            home: const DashBoardScreen(),
-            onGenerateRoute: NavigationManager.generateRoute,
-          );
-        });
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'WetinDeySup',
+      theme: ThemeData(primaryColor: ProjectColors.purple),
+      home: const SplashScreen(),
+    );
   }
 }
