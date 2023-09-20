cd27d53b4fdca8780ea9946b4301ff3be9f8bef2
One-Marvellous <Onebymarvellous@gmai.com>
2023-09-20 04:54:39 +0100

    The Splash screen and the sign-in screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hng_events_app/screens/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,68 +8,13 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color(0xFFF97316),
-          primary: const Color(0xFFF97316),
-          // secondary: const Color(0xFFF97316),
-        ),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(),
+      debugShowCheckedModeBanner: false,
+      title: 'WetinDeySup',
+      theme: ThemeData(primarySwatch: Colors.purple),
+      home: const SplashScreen(),
     );
   }
 }
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    final primaryColor = Theme.of(context).colorScheme.primary;
-
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: primaryColor,
-        title: const Text("Title"),
-      ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CircleAvatar(
-              radius: 50,
-              backgroundImage: AssetImage("assets/images/pfp3.jfif"),
-            ),
-            Text(
-              "data",
-              style: TextStyle(
-                fontSize: 45,
-                fontFamily: "Inter",
-                fontWeight: FontWeight.w900,
-              ),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        backgroundColor: primaryColor,
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-}
-
-
-// PULL TESTTTTTTTTTTTTT 
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:hng_events_app/constants/colors.dart';
+import 'package:hng_events_app/screens/signin_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToSignIn(context);
+  }
+
+  void _navigateToSignIn(BuildContext context) {
+    Future.delayed(const Duration(milliseconds: 1500), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const SignIn()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          color: ProjectColors.bgColor,
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 50,
+              ),
+              Center(
+                child:
+                    Image.asset('assets/illustrations/splash_screen_icon.png'),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              const Center(
+                child: Text('WetinDeySup',
+                    style: TextStyle(
+                        fontSize: 48,
+                        fontFamily: 'Lato',
+                        fontWeight: FontWeight.bold)),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
