941445b5703556d4e75170b98be108b4be5c6424
Henryikenna <ikennaunegbu10@gmail.com>
2023-09-21 13:32:21 +0100

    prudent-migabo-#25

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       title: 'WetinDeySup',
       theme: ThemeData(primaryColor: ProjectColors.purple),
       home: const SplashScreen(),
-      // home: const CreateGroup(),
+      // home: const ChatScreen(),
     );
   }
 }
