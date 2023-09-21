be22e374f7605fe78627abce5ef0ab87a4a68bcb
ClementPeter <clementpeter2000@gmail.com>
2023-09-21 03:18:48 +0100

    feat/http_service

---
+++ lib/models/error_model.dart
@@ -0,0 +1,11 @@
+class ErrorModel {
+  final dynamic _error;
+
+  ErrorModel(this._error);
+
+  @override
+  String toString() {
+    // TODO: implement toString
+    return 'Error Instance $_error';
+  }
+}
