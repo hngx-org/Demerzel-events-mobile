8fdbf3136fbd5ef72c0fd43f56cc7af7932aebc5
Edeh-cj <ceejosh360@gmail.com>
2023-10-02 11:07:01 +0100

    feat: update user info

--- lib/repositories/auth_repository.dart
+++ lib/repositories/auth_repository.dart
@@ -139,5 +139,5 @@ class AuthRepository {
   }
 
   static final provider = Provider<AuthRepository>((ref) => AuthRepository(
-      localStorageService: ref.read(LocalStorageService.provider)));
+    localStorageService: ref.read(LocalStorageService.provider)));
 }
